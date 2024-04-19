a4bde028ac248a721fc01f84b339428354b73a78
Singhary <singhary449@gmail.com>
2024-04-19 11:30:48 +0530

    added prompt editing and deleting facility

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,51 +1,51 @@
-"use client"
-import { useState } from "react" ;
+"use client";
+
+import { useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+
 import Form from "@components/Form";
 
-const createPrompt = () => {
-  const router = useRouter() ;
-  const {data:session} = useSession() ;
+const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
 
-   const [submitting, setsubmitting] = useState(false)
-   const [post, setPost] = useState({
-        prompt:'',
-        tag:'',
-   });
+  const [submitting, setIsSubmitting] = useState(false);
+  const [post, setPost] = useState({ prompt: "", tag: "" });
 
-   const createPrompt = async(e)=>{
-       e.preventDefault() ;
-       setsubmitting(true);
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
 
-       try {
-         const response = await fetch('/api/prompt/new',{
-          method:'POST',
-          body:JSON.stringify({
-             prompt:post.prompt,
-             userId:session?.user.id,
-             tag:post.tag,
-          })
-         })
-         
-         if(response.ok){
-          router.push('/');
-         }
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
 
-       } catch (error) {
-         console.log("There was an error in creating thr prompt!")
-       }
-   }
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
 
   return (
-   <Form
-     type="Create"
-     post = {post}
-     setPost={setPost}
-     submitting={submitting}
-     handelSubmit = {createPrompt}
-   />
- )
-}
+    <Form
+      type='Create'
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
+};
 
-export default createPrompt
+export default CreatePrompt;
--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,48 +1,61 @@
-"use client"
+"use client";
 
-import { useState , useEffect } from "react"
-import { useSession } from "next-auth/react"
-import { useRouter } from "next/router"
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
-import Profile from "@components/Profile"
+import Profile from "@components/Profile";
 
-const page = () => {
-    
-    const {data:session} = useSession() ;
-    const [post , setPost] = useState([]) ;
+const MyProfile = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
 
-    const handelDelete = async ()=>{
-         
+  const [myPosts, setMyPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setMyPosts(data);
+    };
+
+    if (session?.user.id) fetchPosts();
+  }, [session?.user.id]);
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = myPosts.filter((item) => item._id !== post._id);
+
+        setMyPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
     }
-
-    const handelEdit =  ()=>{
-
-    }
-
-    useEffect(()=>{
-        // The purpose of calling response.json() is to extract the JSON body content from the response and parse it into a JavaScript object.
-          const fetchPrompt = async ()=>{
-            const response = await fetch(`/api/users/${session?.user.id}/posts`) ;
-            const data = await response.json() ;
-            setPost(data) ;
-          }
-          if(session?.user.id){
-            fetchPrompt() ;
-          }
-      },[])
-    
+  };
 
   return (
-    <div>
-        <Profile
-            name="My"
-            desc="Welcome to your personalised Profile page"
-            data={post}
-            handelEdit={handelEdit}
-            handelDelete = {handelDelete}
-        />
-    </div>
-  )
-}
+    <Profile
+      name='My'
+      desc='Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination'
+      data={myPosts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+};
 
-export default page
+export default MyProfile;
